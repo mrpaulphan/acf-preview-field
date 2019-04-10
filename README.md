3bb55fe1a5f35bf1bd948819e739387fb4244e2f
Paul Phan <paul@mrpaulphan.com>
2019-04-10 13:42:36 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ACF Preview Field
+
+Welcome to the Advanced Custom Fields Preview repository on Github.
+
+EXTENDED_DESCRIPTION
---
+++ acf-preview.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: Preview
+Plugin URI: PLUGIN_URL
+Description: SHORT_DESCRIPTION
+Version: 1.0.0
+Author: Impact Six
+Author URI: https://impactsix.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) exit;
+
+
+// check if class already exists
+if( !class_exists('impactsix_acf_plugin_preview') ) :
+
+class impactsix_acf_plugin_preview {
+	
+	// vars
+	var $settings;
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the class functionality
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	void
+	*  @return	void
+	*/
+	
+	function __construct() {
+		
+		// settings
+		// - these will be passed into the field class.
+		$this->settings = array(
+			'version'	=> '1.0.0',
+			'url'		=> plugin_dir_url( __FILE__ ),
+			'path'		=> plugin_dir_path( __FILE__ )
+		);
+		
+		
+		// include field
+		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
+		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
+	}
+	
+	
+	/*
+	*  include_field
+	*
+	*  This function will include the field type class
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	$version (int) major ACF version. Defaults to false
+	*  @return	void
+	*/
+	
+	function include_field( $version = false ) {
+		
+		// support empty $version
+		if( !$version ) $version = 4;
+		
+		
+		// load acf-preview
+		load_plugin_acf-preview( 'acf-preview', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
+		
+		
+		// include
+		include_once('fields/class-impactsix-acf-field-preview-v' . $version . '.php');
+	}
+	
+}
+
+
+// initialize
+new impactsix_acf_plugin_preview();
+
+
+// class_exists check
+endif;
+	
+?>
